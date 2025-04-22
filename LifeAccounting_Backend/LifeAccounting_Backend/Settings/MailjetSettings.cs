17b9cba39451ec3a5f9c1c9ad87ec6b00b9a303a
aaronlin <aaronlin@ukn.edu.tw>
2025-04-22 11:23:12 +0800

    add initial settings

---
+++ LifeAccounting_Backend/LifeAccounting_Backend/Settings/MailjetSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace LifeAccounting_Backend.Settings
+{
+    // Mailjet 設定參數
+    public class MailjetSettings
+    {
+        public string ApiKey { get; set; } = null!;
+        public string ApiSecret { get; set; } = null!;
+        public string SenderEmail { get; set; } = null!;
+        public string SenderName { get; set; } = null!;
+    }
+}
--- LifeAccounting_Backend/LifeAccounting_Backend/obj/Debug/net8.0/LifeAccounting_Backend.AssemblyInfo.cs
+++ LifeAccounting_Backend/LifeAccounting_Backend/obj/Debug/net8.0/LifeAccounting_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeAccounting_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67eea6d713a2fe914e2ae9461e68f9db70d3d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeAccounting_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeAccounting_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
